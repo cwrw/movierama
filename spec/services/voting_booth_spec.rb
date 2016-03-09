e137fa13f1c2f39c6b7f3656e2729f229b4102a0
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-09 09:11:40 +0000

    Add missing voting booth spec

---
+++ spec/services/voting_booth_spec.rb
@@ -0,0 +1,64 @@
+require "rails_helper"
+
+RSpec.describe VotingBooth do
+  let(:user) do
+    User.create(
+      uid:  'null|12345',
+      name: 'Alice',
+      email: 'alice@example.com'
+    )
+  end
+
+  let(:author) do
+
+    User.create(
+      uid:  'null|67890',
+      name: 'Bob',
+      email: 'bob@example.com'
+    )
+  end
+
+  let(:m_empire) do
+    Movie.create(
+      title:        'Empire strikes back',
+      description:  'Who\'s scruffy-looking?',
+      date:         '1980-05-21',
+      user:         author,
+      liker_count:  0,
+      hater_count:  0
+    )
+  end
+
+  subject { described_class.new(user, m_empire) }
+
+  describe "vote" do
+    it "increments movie liker_count if vote is a like" do
+      expect{ subject.vote(:like) }.to change { m_empire.liker_count }.from(0).to(1)
+    end
+
+    it "increment movie hater_count if vote is a hate" do
+      expect{ subject.vote(:hate) }.to change { m_empire.hater_count }.from(0).to(1)
+    end
+  end
+
+  describe "unvote" do
+    let(:m_empire) do
+      Movie.create(
+        title:        'Empire strikes back',
+        description:  'Who\'s scruffy-looking?',
+        date:         '1980-05-21',
+        user:         author,
+        liker_count:  1,
+        hater_count:  1
+      )
+    end
+
+    it "dencrement movie liker_count if vote is a like" do
+      expect{ subject.unvote }.to change { m_empire.liker_count }.from(1).to(0)
+    end
+
+    it "decrement movie hater_count if vote is a hate" do
+      expect{ subject.unvote }.to change { m_empire.hater_count }.from(1).to(0)
+    end
+  end
+end
