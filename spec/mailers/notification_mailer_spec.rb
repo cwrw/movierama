b3fcfe38f8325a31b5707660c2c9ee8e1a7f5223
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-09 07:42:30 +0000

    Setup actionmailer, add specs

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe NotificationMailer do
+  describe 'notifier' do
+    let(:author) do
+      User.create(
+        uid:  'null|12345',
+        name: 'Bob',
+        email: 'bob@example.com'
+      )
+    end
+
+    let(:movie) do
+      Movie.create(
+        title:        'Empire strikes back',
+        description:  'Who\'s scruffy-looking?',
+        date:         '1980-05-21',
+        user:         author
+      )
+    end
+
+    let(:action) { "like" }
+    let(:mail) do
+      described_class.vote(
+        user: author,
+        movie: movie,
+        action: action
+      )
+    end
+
+    it 'sets the subject' do
+      expect(mail.subject).to eql('MovieRama notification')
+    end
+
+    it 'sets the receiver email' do
+      expect(mail.to).to eql([author.email])
+    end
+
+    it 'sets the sender email' do
+      expect(mail.from).to eql(['notifications@movierama.com'])
+    end
+
+    it 'sets the correct body with author name' do
+      expect(mail.body.encoded).to match(author.name)
+    end
+
+    it 'sets the correct body with movie and action' do
+      expect(mail.body.encoded).to match("You have received a #{action} vote on your movie: #{movie.title}")
+    end
+  end
+end
